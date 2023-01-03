9bbb3a015e226316e1b168dbd1aae786ae154bad
Vanya1000 <Vanya1000@yandex.ru>
2023-01-03 22:34:15 +0300

    feat: implement class router

---
+++ src/server/Router.ts
@@ -0,0 +1,45 @@
+import { EndpointsType, HandlerType } from "./types.js";
+
+class Router {
+  public endpoints: EndpointsType;
+	constructor() {
+		this.endpoints = {}
+	}
+
+	request(method = 'GET', path = '', handler: HandlerType) {
+		if (path.includes(':')) {
+			path = path.split(':')[0] + ':id'
+		} 
+		if (!this.endpoints[path]) {
+			this.endpoints[path] = {}
+		}
+		const endpoint = this.endpoints[path]
+		if (endpoint[method]) {
+			throw new Error(`Endpoint ${method} ${path} already exists`)
+		}
+		endpoint[method] = handler
+		
+	}
+
+	get(path: string, handler: HandlerType) {
+		this.request('GET', path, handler)
+	}
+
+	post(path: string, handler: HandlerType) {
+		this.request('POST', path, handler)
+	}
+
+	put(path: string, handler: HandlerType) {
+		this.request('PUT', path, handler)
+	}
+
+	delete(path: string, handler: HandlerType) {
+		this.request('DELETE', path, handler)
+	}
+
+	getAllEndpoints() {
+		return this.endpoints
+	}
+}
+
+export default Router
