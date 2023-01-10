9cbc8a7f648a8cab729b48dc053285a72d44bf66
Vanya1000 <Vanya1000@yandex.ru>
2023-01-10 22:26:04 +0300

    feat: add 3 new tests

--- test/users/users.test.ts
+++ test/users/users.test.ts
@@ -7,20 +7,19 @@ const testUser = {
   hobbies: ["test"]
 }
 
+const updateUser = {
+  username: "updated",
+  age: 30,
+  hobbies: ["updated"]
+}
+
 describe("Test Users endpoints", () => {
-  /*   beforeEach(() => {
-    app.listen("5000");
-  }); */
-  /* 
-  afterEach((done) => {
-    app.close();
-    done();
-  }); */
 
   it("should get an empty array users", async () => {
     const response = await request(app.server)
       .get("/api/users")
       .set("Accept", "application/json");
+    expect(response.status).toBe(200);
     expect(response.body).toEqual([]);
   });
 
@@ -29,16 +28,56 @@ describe("Test Users endpoints", () => {
       .post("/api/users")
       .send(testUser)
       .set("Accept", "application/json");
+    expect(response.status).toBe(201);
     expect(response.body).toMatchObject({
       id: expect.any(String),
       ...testUser
     });
   });
 
-/*   it("should get an empty array users", async () => {
+  it("should get user by Id", async () => {
     const response = await request(app.server)
-      .get("/api/users")
+      .post("/api/users")
+      .send(testUser)
       .set("Accept", "application/json");
-    expect(response.body).toEqual([]);
-  }); */
+    const idUser = response.body.id;
+    const responseGetUser = await request(app.server)
+      .get(`/api/users/${idUser}`)
+      .set("Accept", "application/json");
+    expect(responseGetUser.status).toBe(200);
+    expect(responseGetUser.body).toMatchObject({
+      id: idUser,
+      ...testUser
+    });
+  });
+
+  it ("should update user data", async () => {
+    const response = await request(app.server)
+      .post("/api/users")
+      .send(testUser)
+      .set("Accept", "application/json");
+    const idUser = response.body.id;
+    const responseUpdateUser = await request(app.server)
+      .put(`/api/users/${idUser}`)
+      .send(updateUser)
+      .set("Accept", "application/json");
+    expect(responseUpdateUser.status).toBe(200);
+    expect(responseUpdateUser.body).toMatchObject({
+      id: idUser,
+      ...updateUser
+    });
+  });
+
+  it ("should delete user", async () => {
+    const response = await request(app.server)
+      .post("/api/users")
+      .send(testUser)
+      .set("Accept", "application/json");
+    const idUser = response.body.id;
+    const responseDeleteUser = await request(app.server)
+      .delete(`/api/users/${idUser}`)
+      .set("Accept", "application/json");
+    expect(responseDeleteUser.status).toBe(204);
+  });
+
 });
